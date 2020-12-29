24cf405829b93cb2519f94982f35c585cd44933f
lyoh001@hotmail.com <lyoh001@hotmail.com>
2020-12-30 10:14:19 +1100

    Commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -6,7 +6,6 @@ import os
 def lambda_handler(event, context):
     try:
         print(f"Total Number of CPUs: {multiprocessing.cpu_count()}")
-
         return {
             "statusCode": "200",
             "body": f"Total Number of CPUs: {multiprocessing.cpu_count()}"
