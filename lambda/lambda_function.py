873ad76a90447d3fd23ea9aef30ff88755a05f39
lyoh001@hotmail.com <lyoh001@hotmail.com>
2020-12-31 10:05:13 +1100

    Commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
         x, y = float(payload["x"]), float(payload["y"])
         return_val = {" ": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]]
         return {"statusCode": "200", "body": f"Payload: {return_val}"}
-        
+
     except Exception as e:
         print(f"Exception: {e}")
         return {"statusCode": "400", "body": f"Exception: {e}"}
