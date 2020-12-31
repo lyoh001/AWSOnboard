2973335086cdebdaae05f536709647b5b02e5a2f
lyoh001@hotmail.com <lyoh001@hotmail.com>
2020-12-31 23:54:52 +1100

    Commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,23 +1,24 @@
 import boto3
 import datetime
+import glob
 import json
 import os
 import tempfile
 
 
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    print("---------------Starting lambda function---------------")
     print(f"Payload: {(payload := event.get('queryStringParameters'))}")
     try:
         x, y = float(payload["x"]), float(payload["y"])
         answer = {" ": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]]
         with tempfile.TemporaryDirectory() as tempdir_path:
-            path = os.path.join(tempdir_path, (file_name := f"{'.'.join(str(datetime.datetime.now()).split(':'))}.txt"))
+            path = os.path.join(tempdir_path, (file_name := f"{'-'.join('-'.join(str(datetime.datetime.now()).split(':')).split())}.txt"))
             with open(path, "w") as file_writer:
-                file_writer.write(json.dumps(answer, indent=4))
-                s3.meta.client.upload_file(path, os.environ["BUCKET"], file_name)
+                file_writer.write(f"{answer}")
+            print(glob.glob(f"{tempdir_path}/*"))
+            s3.upload_file(path, os.environ["BUCKET"], file_name)
         return {"statusCode": "200", "body": f"Payload: {answer}"}
 
     except Exception as e:
