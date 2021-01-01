9933adb9578d6b1e96a243b4b2889234cc38dfc2
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-01-01 21:07:22 +1100

    Commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -13,14 +13,16 @@ def lambda_handler(event, context):
     try:
         x, y = float(payload["x"]), float(payload["y"])
         payload["z"] = {"+": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]]
-        with tempfile.TemporaryDirectory() as tempdir_path:
-            path = os.path.join(tempdir_path, (file_name := f"{'-'.join('-'.join(str(datetime.datetime.now()).split(':')).split())}.txt"))
-            with open(path, "w") as file_writer:
-                file_writer.write(json.dumps(payload, indent=4))
-            print(glob.glob(f"{tempdir_path}/*"))
-            s3.upload_file(path, os.environ["BUCKET"], file_name)
-        return {"statusCode": "200", "body": json.dumps(payload)}
 
     except Exception as e:
         print(f"Exception: {e}")
         return {"statusCode": "400", "body": f"Exception: {e}"}
+
+    with tempfile.TemporaryDirectory() as tempdir_path:
+        path = os.path.join(tempdir_path, (file_name := f"{'-'.join('-'.join(str(datetime.datetime.now()).split(':')).split())}.txt"))
+        with open(path, "w") as file_writer:
+            file_writer.write(json.dumps(payload, indent=4))
+        print(glob.glob(f"{tempdir_path}/*"))
+        s3.upload_file(path, os.environ["BUCKET"], file_name)
+
+    return {"statusCode": "200", "body": json.dumps(payload)}
