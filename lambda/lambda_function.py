5d73c8321051e2eddb00969cbfd5f36de097bfe4
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-01-01 16:51:58 +1100

    Commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -12,14 +12,14 @@ def lambda_handler(event, context):
     print(f"Payload: {(payload := event.get('queryStringParameters'))}")
     try:
         x, y = float(payload["x"]), float(payload["y"])
-        answer = {" ": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]]
+        payload["z"] = {" ": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]]
         with tempfile.TemporaryDirectory() as tempdir_path:
             path = os.path.join(tempdir_path, (file_name := f"{'-'.join('-'.join(str(datetime.datetime.now()).split(':')).split())}.txt"))
             with open(path, "w") as file_writer:
-                file_writer.write(f"{answer}")
+                file_writer.write(json.dumps(payload, indent=4))
             print(glob.glob(f"{tempdir_path}/*"))
             s3.upload_file(path, os.environ["BUCKET"], file_name)
-        return {"statusCode": "200", "body": f"Payload: {answer}"}
+        return {"statusCode": "200", "body": f"Payload: {payload}"}
 
     except Exception as e:
         print(f"Exception: {e}")
