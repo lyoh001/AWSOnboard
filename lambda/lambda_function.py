fd8b8291831b481aabc2fc4c4a0088b5f090483a
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-01-11 06:33:30 +1100

    Commit

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,39 +1,39 @@
-# import boto3
-# import datetime
-# import glob
-# import json
-# import os
-# import tempfile
-
-
-# s3 = boto3.client('s3')
-
-# def lambda_handler(event, context):
-#     print(f"Payload: {(payload := event.get('queryStringParameters'))}")
-#     try:
-#         x, y = float(payload["x"]), float(payload["y"])
-#         payload["z"] = {"+": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]]
-
-#     except Exception as e:
-#         print(f"Exception: {e}")
-#         return {"statusCode": "400", "body": f"Exception: {e}"}
-
-#     with tempfile.TemporaryDirectory() as tempdir_path:
-#         path = os.path.join(tempdir_path, (file_name := f"{'-'.join('-'.join(str(datetime.datetime.now()).split(':')).split())}.txt"))
-#         with open(path, "w") as file_writer:
-#             file_writer.write(json.dumps(payload, indent=4))
-#         print(glob.glob(f"{tempdir_path}/*"))
-#         s3.upload_file(path, os.environ["BUCKET"], file_name)
-
-#     return {"statusCode": "200", "body": json.dumps(payload)}
-
 import boto3
+import datetime
+import glob
 import json
+import os
+import tempfile
+
+
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    response = "Received Message Body from API GW: " + event['Records'][0]['body']
-    print(response)
-    return {
-        'statusCode': 200,
-        'body': response
-    }
+    print(f"Payload: {(payload := event.get('queryStringParameters'))}")
+    try:
+        x, y = float(payload["x"]), float(payload["y"])
+        payload["z"] = {"+": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]]
+
+    except Exception as e:
+        print(f"Exception: {e}")
+        return {"statusCode": "400", "body": f"Exception: {e}"}
+
+    with tempfile.TemporaryDirectory() as tempdir_path:
+        path = os.path.join(tempdir_path, (file_name := f"{'-'.join('-'.join(str(datetime.datetime.now()).split(':')).split())}.txt"))
+        with open(path, "w") as file_writer:
+            file_writer.write(json.dumps(payload, indent=4))
+        print(glob.glob(f"{tempdir_path}/*"))
+        s3.upload_file(path, os.environ["BUCKET"], file_name)
+
+    return {"statusCode": "200", "body": json.dumps(payload)}
+
+# import boto3
+# import json
+
+# def lambda_handler(event, context):
+#     response = "Received Message Body from API GW: " + event['Records'][0]['body']
+#     print(response)
+#     return {
+#         'statusCode': 200,
+#         'body': response
+#     }
