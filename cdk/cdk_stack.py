db3dde17a4e46072aa96087708aa341fa9f2b2a7
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-01-11 12:44:05 +1100

    Commit

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -1,35 +1,46 @@
-# from aws_cdk import (
-#     aws_apigateway as apigateway,
-#     aws_dynamodb as dynamodb,
-#     aws_ec2 as ec2,
-#     aws_iam as iam,
-#     aws_lambda as lambda_,
-#     aws_s3 as s3,
-#     aws_sns as sns,
-#     aws_sns_subscriptions as subs,
-#     aws_sqs as sqs,
-#     core
-# )
+from aws_cdk import aws_apigateway as apigateway
 from aws_cdk import aws_dynamodb as dynamodb
 from aws_cdk import aws_lambda as lambda_
 from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_sns as sns
+from aws_cdk import aws_sns_subscriptions as subs
+from aws_cdk import aws_sqs as sqs
 from aws_cdk import core
 
 
 class CdkStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # prefix for resource names
         prefix = "vickk73"
-        table = dynamodb.Table(self, f"{prefix}dynamodb", partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING), removal_policy=core.RemovalPolicy.DESTROY)
+
+        # creating s3 bucket
         bucket = s3.Bucket(self, f"{prefix}s3", removal_policy=core.RemovalPolicy.DESTROY)
-        handler = lambda_.Function(self, f"{prefix}lambda", runtime=lambda_.Runtime.PYTHON_3_8, handler="lambda_function.lambda_handler", code=lambda_.Code.asset("lambda"), environment=dict(BUCKET=bucket.bucket_name, TABLE_NAME=table.table_name))
-        # table.grant_read_write_data(handler)
-        # bucket.grant_read_write(handler)
-        # api = apigateway.RestApi(self, f"{prefix}api", rest_api_name=f"{prefix}api", description=f"{prefix} rest api gateway.")
-        # api.root.add_method("GET", apigateway.LambdaIntegration(handler, request_templates={"application/json": '{ "statusCode": "200" }'}))
-        # queue = sqs.Queue(self, f"{prefix}sqs", visibility_timeout=core.Duration.seconds(300))
-        # topic = sns.Topic(self, f"{prefix}sns")
-        # topic.add_subscription(subs.SqsSubscription(queue))
+
+        # creating dynamodb
+        table = dynamodb.Table(self, f"{prefix}dynamodb", partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING), removal_policy=core.RemovalPolicy.DESTROY)
+
+        # creating sqs
+        queue = sqs.Queue(self, f"{prefix}sqs", visibility_timeout=core.Duration.seconds(300))
+
+        # creating sns and sns sub
+        topic = sns.Topic(self, f"{prefix}sns", display_name=f"{prefix} sns")
+        topic.add_subscription(subs.SmsSubscription(phone_number="61410844028"))
+
+        # creating lambda
+        handler = lambda_.Function(self, f"{prefix}lambda", runtime=lambda_.Runtime.PYTHON_3_8, handler="lambda_function.lambda_handler", code=lambda_.Code.asset("lambda"), environment=dict(BUCKET_NAME=bucket.bucket_name, TABLE_NAME=table.table_name, SNS_ARN=topic.topic_arn))
+
+        # creating and defining iam role permission
+        bucket.grant_read_write(handler)
+        table.grant_read_write_data(handler)
+        topic.grant_publish(handler)
+
+        # creating api gateway
+        api = apigateway.RestApi(self, f"{prefix}api", rest_api_name=f"{prefix}api", description=f"{prefix} rest api gateway.", endpoint_configuration={"types": [apigateway.EndpointType.REGIONAL]})
+        api.root.add_method("GET", apigateway.LambdaIntegration(handler, request_templates={"application/json": '{ "statusCode": "200" }'}))
+
+        # creating vpc
         # vpc = ec2.Vpc(self, f"{prefix}vpc", cidr="10.0.0.0/16")
         # selection = vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE)
         # for subnet in selection.subnets:
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -6,34 +6,48 @@ import os
 import tempfile
 
 
-s3 = boto3.client('s3')
+dynamodb_resource = boto3.resource("dynamodb")
+s3_client = boto3.client('s3')
+sns_client = boto3.client("sns")
 
 def lambda_handler(event, context):
+    # getting the payload
     print(f"Payload: {(payload := event.get('queryStringParameters'))}")
+
+    # calculating input
     try:
         x, y = float(payload["x"]), float(payload["y"])
-        payload["z"] = {"+": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]]
+        payload["z"] = str({"+": x+y, "-": x-y, "*": x*y, "/": x/y}[payload["op"]])
 
     except Exception as e:
         print(f"Exception: {e}")
         return {"statusCode": "400", "body": f"Exception: {e}"}
 
+    # writing to dynamodb table
+    table = dynamodb_resource.Table(os.environ["TABLE_NAME"])
+    response = table.put_item(Item={
+        "id": str(datetime.datetime.now()),
+        "data": payload
+    })
+
+    # sending response to sns
+    sns_client.publish(
+        TopicArn=os.environ["SNS_ARN"],
+        Message=f"HTTP Response: {response['ResponseMetadata']['HTTPStatusCode']}\nAnswer: {payload['z']}",
+        MessageAttributes={
+            "lambdamessage": {
+                "DataType": "String",
+                "StringValue": "true"
+            }
+        }
+    )
+
+    # writing to s3 bucket
     with tempfile.TemporaryDirectory() as tempdir_path:
         path = os.path.join(tempdir_path, (file_name := f"{'-'.join('-'.join(str(datetime.datetime.now()).split(':')).split())}.txt"))
         with open(path, "w") as file_writer:
             file_writer.write(json.dumps(payload, indent=4))
         print(glob.glob(f"{tempdir_path}/*"))
-        s3.upload_file(path, os.environ["BUCKET"], file_name)
+        s3_client.upload_file(path, os.environ["BUCKET_NAME"], file_name)
 
     return {"statusCode": "200", "body": json.dumps(payload)}
-
-# import boto3
-# import json
-
-# def lambda_handler(event, context):
-#     response = "Received Message Body from API GW: " + event['Records'][0]['body']
-#     print(response)
-#     return {
-#         'statusCode': 200,
-#         'body': response
-#     }
