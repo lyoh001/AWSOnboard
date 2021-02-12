63b92bc61830ed3cc4824973c1c5d81c994555e3
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-02-12 15:43:45 +1100

    Commit

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -93,37 +93,37 @@ class CdkStack(core.Stack):
             parameter_group=rds.ParameterGroup.from_parameter_group_name(
                 self, "ParameterGroup", "default.aurora-postgresql10"
             ),
-            default_database_name="db",
+            # default_database_name="db",
             vpc=vpc,
         )
 
-        # creating lambda
-        handler = lambda_.Function(
-            self,
-            f"lambda{prefix}",
-            runtime=lambda_.Runtime.PYTHON_3_8,
-            code=lambda_.Code.asset("lambda"),
-            handler="lambda_function.lambda_handler",
-            memory_size=1024,
-            environment=dict(
-                CLUSTER_ARN=cluster.cluster_arn,
-                SECRET_ARN=cluster.secret.secret_arn,
-                DB_NAME="db",
-                AWS_PYTHON_CONNECTION_REUSE_ENABLED="1",
-            ),
-        )
+        # # creating lambda
+        # handler = lambda_.Function(
+        #     self,
+        #     f"lambda{prefix}",
+        #     runtime=lambda_.Runtime.PYTHON_3_8,
+        #     code=lambda_.Code.asset("lambda"),
+        #     handler="lambda_function.lambda_handler",
+        #     memory_size=1024,
+        #     environment=dict(
+        #         CLUSTER_ARN=cluster.cluster_arn,
+        #         SECRET_ARN=cluster.secret.secret_arn,
+        #         DB_NAME="db",
+        #         AWS_PYTHON_CONNECTION_REUSE_ENABLED="1",
+        #     ),
+        # )
 
-        # creating and defining iam role permission
-        cluster.grant_data_api_access(handler)
+        # # creating and defining iam role permission
+        # cluster.grant_data_api_access(handler)
 
-        # creating api gateway
-        api = apigatewayv2.HttpApi(
-            self,
-            f"end_point{prefix}",
-            default_integration=apigatewayv2_integrations.LambdaProxyIntegration(
-                handler=handler
-            ),
-        )
+        # # creating api gateway
+        # api = apigatewayv2.HttpApi(
+        #     self,
+        #     f"end_point{prefix}",
+        #     default_integration=apigatewayv2_integrations.LambdaProxyIntegration(
+        #         handler=handler
+        #     ),
+        # )
 
         # cfn output
         core.CfnOutput(self, "api_url", value=api.url)
