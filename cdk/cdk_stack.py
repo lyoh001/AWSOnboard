387150a5ac61c6443e44859172ebeb8664d291e6
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-02-19 18:19:58 +1100

    Commit

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -5,6 +5,7 @@ from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_sns as sns
 from aws_cdk import aws_sns_subscriptions as subs
 from aws_cdk import aws_sqs as sqs
+from aws_cdk import aws_wafv2 as waf
 from aws_cdk import core
 
 
@@ -68,6 +69,111 @@ class CdkStack(core.Stack):
                 handler,
                 request_templates={"application/json": '{ "statusCode": "200" }'},
             ),
+        )
+
+        # creating waf
+        waf_rules = []
+        # 1, AWS general rules
+        aws_managed_rules = waf.CfnWebACL.RuleProperty(
+            name="AWS-AWSManagedRulesCommonRuleSet",
+            priority=1,
+            override_action=waf.CfnWebACL.OverrideActionProperty(none={}),
+            statement=waf.CfnWebACL.StatementOneProperty(
+                managed_rule_group_statement=waf.CfnWebACL.ManagedRuleGroupStatementProperty(
+                    name="AWSManagedRulesCommonRuleSet",
+                    vendor_name="AWS",
+                    excluded_rules=[
+                        waf.CfnWebACL.ExcludedRuleProperty(name="SizeRestrictions_BODY")
+                    ],
+                )
+            ),
+            visibility_config=waf.CfnWebACL.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                metric_name="awsCommonRules",
+                sampled_requests_enabled=True,
+            ),
+        )
+        waf_rules.append(aws_managed_rules)
+
+        # 2, AWS AnonIPAddress
+        aws_anoniplist = waf.CfnWebACL.RuleProperty(
+            name="awsAnonymousIP",
+            priority=2,
+            override_action=waf.CfnWebACL.OverrideActionProperty(none={}),
+            statement=waf.CfnWebACL.StatementOneProperty(
+                managed_rule_group_statement=waf.CfnWebACL.ManagedRuleGroupStatementProperty(
+                    name="AWSManagedRulesAnonymousIpList",
+                    vendor_name="AWS",
+                    excluded_rules=[],
+                )
+            ),
+            visibility_config=waf.CfnWebACL.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                metric_name="awsAnonymous",
+                sampled_requests_enabled=True,
+            ),
+        )
+        waf_rules.append(aws_anoniplist)
+
+        # 3 AWS ip reputation List
+        aws_ip_rep_list = waf.CfnWebACL.RuleProperty(
+            name="aws_Ipreputation",
+            priority=3,
+            override_action=waf.CfnWebACL.OverrideActionProperty(none={}),
+            statement=waf.CfnWebACL.StatementOneProperty(
+                managed_rule_group_statement=waf.CfnWebACL.ManagedRuleGroupStatementProperty(
+                    name="AWSManagedRulesAmazonIpReputationList",
+                    vendor_name="AWS",
+                    excluded_rules=[],
+                )
+            ),
+            visibility_config=waf.CfnWebACL.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                metric_name="aws_reputation",
+                sampled_requests_enabled=True,
+            ),
+        )
+        waf_rules.append(aws_ip_rep_list)
+
+        # 4 GeoBlock NZ from accessing gateway
+        geoblock_rule = waf.CfnWebACL.RuleProperty(
+            name="geoblocking_rule",
+            priority=4,
+            action=waf.CfnWebACL.RuleActionProperty(block={}),
+            statement=waf.CfnWebACL.StatementOneProperty(
+                geo_match_statement=waf.CfnWebACL.GeoMatchStatementProperty(
+                    country_codes=["NZ"],
+                )
+            ),
+            visibility_config=waf.CfnWebACL.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                metric_name="geoblock",
+                sampled_requests_enabled=True,
+            ),
+        )
+        waf_rules.append(geoblock_rule)
+
+        # Create the Waf ACL
+        WebACL = waf.CfnWebACL(
+            self,
+            f"{prefix}webacl",
+            default_action=waf.CfnWebACL.DefaultActionProperty(allow={}),
+            scope="REGIONAL",  # vs 'CLOUDFRONT'
+            visibility_config=waf.CfnWebACL.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                metric_name="webACL",
+                sampled_requests_enabled=True,
+            ),
+            name=f"{prefix}webacl",
+            rules=waf_rules,
+        )
+
+        # Associate it with the resource provided.
+        waf.CfnWebACLAssociation(
+            self,
+            f"{prefix}waf",
+            web_acl_arn=WebACL.attr_arn,
+            resource_arn=api.arn_for_execute_api(),
         )
 
         # creating and defining iam role permission
