fb48f97e772019629cd59463b8231194eed35537
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-02-20 14:45:53 +1100

    Commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ serverlessdynamo = core.App()
 ServerlessDynamo(serverlessdynamo, "serverlessdynamo", env={"region": "ap-southeast-2"})
 serverlessdynamo.synth()
 
-# ServerlessAurora Stack
-serverlessaurora = core.App()
-ServerlessAurora(serverlessaurora, "serverlessaurora", env={"region": "ap-southeast-2"})
-serverlessaurora.synth()
+# # ServerlessAurora Stack
+# serverlessaurora = core.App()
+# ServerlessAurora(serverlessaurora, "serverlessaurora", env={"region": "ap-southeast-2"})
+# serverlessaurora.synth()
