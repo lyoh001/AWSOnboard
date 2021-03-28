f4b5f5906774aa868a7e0020c7dabec0df8080c3
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-03-28 14:26:02 +1100

    Commit

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ from aws_cdk import core
 
 from cdk.serverless_aurora import ServerlessAurora
 from cdk.serverless_dynamo import ServerlessDynamo
+from cdk.serverless_test import ServerlessTest
+
+# # ServerlessTest Stack
+serverlesstest = core.App()
+ServerlessTest(serverlesstest, "serverlesstest", env={"region": "ap-southeast-2"})
+serverlesstest.synth()
 
 # # ServerlessDynamo Stack
-serverlessdynamo = core.App()
-ServerlessDynamo(serverlessdynamo, "serverlessdynamo", env={"region": "ap-southeast-2"})
-serverlessdynamo.synth()
+# serverlessdynamo = core.App()
+# ServerlessDynamo(serverlessdynamo, "serverlessdynamo", env={"region": "ap-southeast-2"})
+# serverlessdynamo.synth()
 
 
 # # ServerlessAurora Stack
---
+++ cdk/serverless_test.py
@@ -0,0 +1,14 @@
+from aws_cdk import aws_s3 as s3
+from aws_cdk import core
+
+
+class ServerlessTest(core.Stack):
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        prefix = "apse2serverlesstest"
+
+        # creating s3 bucket
+        bucket = s3.Bucket(
+            self, f"s3{prefix}", removal_policy=core.RemovalPolicy.DESTROY
+        )
