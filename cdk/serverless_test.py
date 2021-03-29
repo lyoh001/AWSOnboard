eb20baac6544de5b8a76507a99fdff29ae272006
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-03-29 16:43:40 +1100

    Commit

--- cdk/serverless_test.py
+++ cdk/serverless_test.py
@@ -1,3 +1,5 @@
+from attr import Attribute
+from aws_cdk import aws_dynamodb as dynamodb
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_sqs as sqs
@@ -8,18 +10,26 @@ class ServerlessTest(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        prefix = "vickk73stackv1"
+        prefix = "vickk73stackv2"
 
-        # creating s3 bucket
-        bucket = s3.Bucket(
-            self, f"{prefix}s3", removal_policy=core.RemovalPolicy.DESTROY
-        )
+        # # creating s3 bucket
+        # bucket = s3.Bucket(
+        #     self, f"{prefix}s3", removal_policy=core.RemovalPolicy.DESTROY
+        # )
 
-        # creating sqs
-        queue = sqs.Queue(
+        # # creating sqs
+        # queue = sqs.Queue(
+        #     self,
+        #     f"{prefix}sqs",
+        #     visibility_timeout=core.Duration.seconds(300),
+        #     removal_policy=core.RemovalPolicy.DESTROY,
+        # )
+
+        # creating dynamodb
+        table = dynamodb.Table(
             self,
-            f"{prefix}sqs",
-            visibility_timeout=core.Duration.seconds(300),
+            f"{prefix}dynamodb",
+            partition_key=Attribute(name="id", type=dynamodb.AttributeType.STRING),
             removal_policy=core.RemovalPolicy.DESTROY,
         )
 
