f974c51b8a42ccf74f92ab1537cfb7e94766ee05
lyoh001@hotmail.com <lyoh001@hotmail.com>
2022-04-17 15:21:42 +1000

    Commit

--- awscdk/serverless_test.py
+++ awscdk/serverless_test.py
@@ -9,21 +9,45 @@ class ServerlessTest(core.Stack):
 
         prefix = "serverlesstest"
 
-        # creating s3 bucket
-        bucket = s3.Bucket(
-            self,
-            f"{prefix}s3",
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
-            encryption=s3.BucketEncryption.KMS,
-            removal_policy=core.RemovalPolicy.DESTROY,
-        )
+        # # creating s3 bucket
+        # bucket = s3.Bucket(
+        #     self,
+        #     f"{prefix}s3",
+        #     block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+        #     encryption=s3.BucketEncryption.KMS,
+        #     removal_policy=core.RemovalPolicy.DESTROY,
+        # )
 
-        # creating lambda
-        handler = lambda_.Function(
+        # # creating lambda
+        # handler = lambda_.Function(
+        #     self,
+        #     f"lambda{prefix}",
+        #     runtime=lambda_.Runtime.PYTHON_3_8,
+        #     code=lambda_.Code.asset("lambda"),
+        #     handler="lambda_function.lambda_handler",
+        #     memory_size=1024,
+        # )
+
+        from aws_cdk import aws_apigatewayv2 as apigatewayv2
+        from aws_cdk import aws_apigatewayv2_integrations as apigatewayv2_integrations
+        from aws_cdk import aws_ec2 as ec2
+        from aws_cdk import aws_lambda as lambda_
+        from aws_cdk import aws_rds as rds
+        from aws_cdk import core
+
+        # prefix for resource name
+        prefix = "serverlessaurora"
+
+        # creating vpc
+        vpc = ec2.Vpc(self, f"vpc{prefix}")
+
+        # creating aurora cluster
+        cluster = rds.ServerlessCluster(
             self,
-            f"lambda{prefix}",
-            runtime=lambda_.Runtime.PYTHON_3_8,
-            code=lambda_.Code.asset("lambda"),
-            handler="lambda_function.lambda_handler",
-            memory_size=1024,
+            f"auroracluster{prefix}",
+            engine=rds.DatabaseClusterEngine.AURORA_MYSQL,
+            enable_data_api=True,
+            removal_policy=core.RemovalPolicy.DESTROY,
+            deletion_protection=False,
+            vpc=vpc,
         )
